6216383080c721c8d0dffa5389ae35f2edc35328
abc <abc@abcs-MacBook-Pro-2.local>
2016-09-19 23:25:52 +0100

    Boosted

--- redHat.py
+++ redHat.py
@@ -47,26 +47,79 @@ pactTest= pd.merge(people, te, on='people_id')
 print pactTest.shape, pactTrain.shape
 
 #feature selection
+
 from sklearn.feature_selection import SelectPercentile, f_classif, SelectFromModel
 #ftp = SelectPercentile(f_classif, percentile=80)
 from sklearn.svm import LinearSVC
 lsvc = LinearSVC(C=0.01, penalty="l1", dual=False)
 ftp = SelectFromModel(lsvc)
-pactTrain = ftp.fit_transform(pactTrain.values, actTr.outcome)
+pactTrainFeatured = ftp.fit_transform(pactTrain.values, actTr.outcome)
 features = ftp.get_support(indices = False)
 c =[]
 for i in range(len(features)): c.append(i) if ( features[i] == False) else 0
+columNames = list(pactTest.columns.values)
+print pactTest.columns[c]
 pactTest.drop(pactTest.columns[c],axis = 1,inplace= True)
-print pactTest.shape, pactTrain.shape
+print pactTest.shape, pactTrainFeatured.shape
 
+#activityTrain =np.array(pactTrain.activity_id)
+#activityTrain = activityTrain.reshape(len(pactTrainFeatured),1)
+#print activityTrain.shape, '\n'
+#pactTrainFeatured = np.append(pactTrainFeatured,activityTrain,axis = 1 )
+#pactTest['activity_id'] = te.activity_id
+#print pactTest.shape,pactTrainFeatured.shape
+#
+#peopleTrain =np.array(pactTrain.people_id)
+#peopleTrain = peopleTrain.reshape(len(pactTrainFeatured),1)
+#print peopleTrain.shape, '\n'
+#pactTrainFeatured = np.append(pactTrainFeatured,peopleTrain,axis = 1 )
+#pactTest['people_id'] = te.people_id
 
-#train model
+import xgboost as xgb
+dtrain = xgb.DMatrix(pactTrainFeatured,label=actTr.outcome)
+dtest = xgb.DMatrix(pactTest.values)
+param = {'max_depth':10, 'eta':0.02, 'silent':1, 'objective':'binary:logistic' }
+param['nthread'] = 4
+param['eval_metric'] = 'auc'
+param['subsample'] = 0.7
+param['colsample_bytree']= 0.7
+param['min_child_weight'] = 0
+param['booster'] = "gblinear"
+watchlist  = [(dtrain,'train')]
+num_round = 300
+early_stopping_rounds=10
+bst = xgb.train(param, dtrain, num_round ,watchlist,early_stopping_rounds=early_stopping_rounds)
+#   PCA
+#from sklearn.decomposition import PCA
+#pca = PCA(n_components = 38 )
+#pactTrainFeatured   = pca.fit_transform(pactTrainFeatured)
+#xTest = pca.transform(pactTest.values)
+print pactTrainFeatured.shape
+#
+##CROSS VALIDATION
+#from sklearn import cross_validation
+#pactTrainFeaturedCross,pactTestFeatured, y_Train, y_Test = cross_validation.train_test_split(pactTrainFeatured,actTr.outcome,test_size = 0.9,random_state = 0)
+##   train model
+#from sklearn.ensemble import RandomForestClassifier
+#clf = RandomForestClassifier(n_estimators=50)
+#clf = svm.SVC(probability = False)
+#clf.fit(pactTrainFeaturedCross, y_Train)
+##prob = clf.predict_proba(pactTest.values)
+#y_pred = clf.predict_proba(pactTestFeatured)
+#from sklearn.metrics import roc_auc_score
+#print roc_auc_score(y_Test,y_pred[:,1])
+#
+#sys.exit()
+
+# TRAIN REAL MODEL
 from sklearn.ensemble import RandomForestClassifier
 clf = RandomForestClassifier(n_estimators=50)
-clf.fit(pactTrain, actTr.outcome)
-prob = clf.predict_proba(pactTest.values)
 
+clf.fit(pactTrainFeatured, actTr.outcome)
+prob = clf.predict_proba(pactTest.values)
+#prob = clf.predict_proba(xTest)
+xg = bst.predict(dtest)
 #make submission file
-output = pd.DataFrame({ 'activity_id' : actTe.activity_id, 'outcome': prob[:,1] })
+output = pd.DataFrame({ 'activity_id' : actTe.activity_id, 'outcome': (prob[:,1] + xg)/2 })
 output.to_csv('sollution.csv', index = False)
 
